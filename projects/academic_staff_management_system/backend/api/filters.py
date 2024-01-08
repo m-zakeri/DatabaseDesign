1bf21bbb78492fd0c062d1210b0d02e26baacdc6
Younes Vm <younesvm@gmail.com>
2024-01-08 09:27:02 +0330

    Updated filters.py

--- projects/academic_staff_management_system/backend/api/filters.py
+++ projects/academic_staff_management_system/backend/api/filters.py
@@ -208,34 +208,167 @@ class OfficeFilter(filters.FilterSet):
 
 
 class EmployeeFilter(filters.FilterSet):
-    
+    person__user__username = filters.CharFilter(
+        field_name="person__user__username", lookup_expr="iexact", label="Username"
+    )
+    person__user__email = filters.CharFilter(
+        field_name="person__user__email", lookup_expr="exact", label="Email Address"
+    )
+    person__user__first_name = filters.CharFilter(
+        field_name="person__user__first_name", lookup_expr="icontains", label="First Name"
+    )
+    person__user__last_name = filters.CharFilter(
+        field_name="person__user__last_name", lookup_expr="icontains", label="Last Name"
+    )
+    person__persian_first_name = filters.CharFilter(
+        field_name='person__persian_first_name', lookup_expr='icontains', label='Persian First Name'
+    )
+    person__persian_last_name = filters.CharFilter(
+        field_name='person__persian_last_name', lookup_expr='icontains', label='Persian Last Name'
+    )
+    office__department__name = filters.CharFilter(
+        field_name="office__department__name", lookup_expr="icontains", label="Department Name"
+    )
+
     class Meta:
         model = models.Employee
-        fields = "__all__"
+        fields = [
+            "person",
+            "person__user__username",
+            "person__user__email",
+            "person__user__first_name",
+            "person__user__last_name",
+            "person__persian_first_name",
+            "person__persian_last_name",
+            "salary",
+            "hire_date",
+            "office_hours",
+            "status",
+            "office",
+            "office__department__name",
+            "is_committee"
+        ]
 
 
 class FieldFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name="name", lookup_expr="icontains", label="Name"
+    )
+    department__name = filters.CharFilter(
+        field_name="department__name", lookup_expr="icontains", label="Department Name"
+    )
 
     class Meta:
         model = models.Field
-        fields = "__all__"
+        fields = [
+            "name",
+            "department__name",
+            "department",
+            "head"
+        ]
 
 
 class ProfessorFilter(filters.FilterSet):
-    
+    employee__person__user__username = filters.CharFilter(
+        field_name="employee__person__user__username", lookup_expr="iexact", label="Username"
+    )
+    employee__person__user__email = filters.CharFilter(
+        field_name="employee__person__user__email", lookup_expr="exact", label="Email Address"
+    )
+    employee__person__user__first_name = filters.CharFilter(
+        field_name="employee__person__user__first_name", lookup_expr="icontains", label="First Name"
+    )
+    employee__person__user__last_name = filters.CharFilter(
+        field_name="employee__person__user__last_name", lookup_expr="icontains", label="Last Name"
+    )
+    employee__person__persian_first_name = filters.CharFilter(
+        field_name='person__persian_first_name', lookup_expr='icontains', label='Persian First Name'
+    )
+    employee__person__persian_last_name = filters.CharFilter(
+        field_name='employee__person__persian_last_name', lookup_expr='icontains', label='Persian Last Name'
+    )
+    employee__office__department__name = filters.CharFilter(
+        field_name="employee__office__department__name", lookup_expr="icontains", label="Department Name"
+    )
+    employee__office__phone_number = filters.CharFilter(
+        field_name="employee__office__phone_number", lookup_expr="icontains", label="Office Phone Number"
+    )
+    field__name = filters.CharFilter(
+        field_name="field__name", lookup_expr="icontains", label="Field Name"
+    )
+
     class Meta:
         model = models.Professor
-        fields = "__all__"
+        fields = [
+            "employee",
+            "employee__person",
+            "employee__person__user__username",
+            "employee__person__user__email",
+            "employee__person__user__first_name",
+            "employee__person__user__last_name",
+            "employee__person__persian_first_name",
+            "employee__person__persian_last_name",
+            "employee__office__department__name",
+            "employee__office",
+            "employee__office__phone_number",
+            "rank",
+            "field",
+            "field__name",
+            "is_in_committee",
+        ]
 
 
 class ResearcherFilter(filters.FilterSet):
+    employee__person__user__username = filters.CharFilter(
+        field_name="employee__person__user__username", lookup_expr="iexact", label="Username"
+    )
+    employee__person__user__email = filters.CharFilter(
+        field_name="employee__person__user__email", lookup_expr="exact", label="Email Address"
+    )
+    employee__person__user__first_name = filters.CharFilter(
+        field_name="employee__person__user__first_name", lookup_expr="icontains", label="First Name"
+    )
+    employee__person__user__last_name = filters.CharFilter(
+        field_name="employee__person__user__last_name", lookup_expr="icontains", label="Last Name"
+    )
+    employee__person__persian_first_name = filters.CharFilter(
+        field_name='person__persian_first_name', lookup_expr='icontains', label='Persian First Name'
+    )
+    employee__person__persian_last_name = filters.CharFilter(
+        field_name='employee__person__persian_last_name', lookup_expr='icontains', label='Persian Last Name'
+    )
+    employee__office__department__name = filters.CharFilter(
+        field_name="employee__office__department__name", lookup_expr="icontains", label="Department Name"
+    )
+    employee__office__phone_number = filters.CharFilter(
+        field_name="employee__office__phone_number", lookup_expr="icontains", label="Office Phone Number"
+    )
+    field__name = filters.CharFilter(
+        field_name="field__name", lookup_expr="icontains", label="Field Name"
+    )
 
     class Meta:
         model = models.Researcher
-        fields = "__all__"
+        fields = [
+            "employee",
+            "employee__person",
+            "employee__person__user__username",
+            "employee__person__user__email",
+            "employee__person__user__first_name",
+            "employee__person__user__last_name",
+            "employee__person__persian_first_name",
+            "employee__person__persian_last_name",
+            "employee__office__department__name",
+            "employee__office",
+            "employee__office__phone_number",
+            "field",
+            "field__name",
+        ]
 
 
 class ResearchFilter(filters.FilterSet):
+    title = filters.CharFilter(
+        field_name="title", lookup_expr="icontains", label="Title")
 
     class Meta:
         model = models.Research
@@ -243,6 +376,8 @@ class ResearchFilter(filters.FilterSet):
 
 
 class ResearchMemberFilter(filters.FilterSet):
+    role = filters.CharFilter(
+        field_name="role", lookup_expr="icontains", label="Role")
 
     class Meta:
         model = models.ResearchMember
@@ -250,13 +385,14 @@ class ResearchMemberFilter(filters.FilterSet):
 
 
 class ScheduleFilter(filters.FilterSet):
-    
     class Meta:
         model = models.Schedule
         fields = "__all__"
 
 
 class LaboratoryFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name="name", lookup_expr="icontains", label="Name")
 
     class Meta:
         model = models.Laboratory
@@ -264,6 +400,8 @@ class LaboratoryFilter(filters.FilterSet):
 
 
 class LibraryFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name="name", lookup_expr='icontains', label='Name')
 
     class Meta:
         model = models.Library
