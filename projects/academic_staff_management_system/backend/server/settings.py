b82ef1868dd2b989be5ff254b671d48c1b8a805c
AmirHitavi <a.hitavi@yahoo.com>
2024-01-09 01:52:14 +0330

    Added session authentication settting

--- projects/academic_staff_management_system/backend/server/settings.py
+++ projects/academic_staff_management_system/backend/server/settings.py
@@ -137,9 +137,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-
+    
 }
