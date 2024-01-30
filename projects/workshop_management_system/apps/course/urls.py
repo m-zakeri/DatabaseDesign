ecb883f8c201b48d261e29977d090b07a96b4c8f
REZA.SH600 <rezasharafdini973@gmail.com>
2024-01-30 18:10:15 +0330

    Implementation of course component page

--- projects/workshop_management_system/apps/course/urls.py
+++ projects/workshop_management_system/apps/course/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'course_app'
 urlpatterns = [
     path('list/', views.CourseListView.as_view(), name='course_list'),
-    path('like/<int:id>/', views.LikeCoureseView.as_view(), name='like_course')
+    path('detail/<slug:slug>/', views.CourseDetailView.as_view(), name='course_detail'),
+    path('like/<int:id>/', views.LikeCourseView.as_view(), name='like_course'),
+    path('like/comment/<int:id>/', views.LikeCourseCommentView.as_view(), name='like_comment')
 
 ]
--- projects/workshop_management_system/apps/course/views.py
+++ projects/workshop_management_system/apps/course/views.py
@@ -1,16 +1,18 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse, Http404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse
 
 from . import models, madul
+from apps.account.models import User
 
 
 class CourseListView(ListView):
     model = models.Course
     queryset = models.Course.objects.filter(is_publish=True).order_by('-created_at')
-    paginate_by = 2
+    paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,12 +24,62 @@ class CourseListView(ListView):
         return madul.filter_course(self.request, queryset)
 
 
-class LikeCoureseView(View, LoginRequiredMixin):
+class CourseDetailView(DetailView):
+    model = models.Course
+    queryset = models.Course.objects.filter(is_publish=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = get_object_or_404(models.Course, slug=self.kwargs['slug']).category.all()
+        suggested_courses = models.Course.objects.filter(is_publish=True, category__in=categories).distinct().exclude(
+            slug=self.kwargs['slug'])
+        last_two_courses = models.Course.objects.filter(is_publish=True).order_by('-created_at')[:2]
+
+        context['suggested_courses'] = suggested_courses
+        context['last_two_courses'] = last_two_courses
+        return context
+
+    def post(self, request, **kwargs):
+        message = request.POST.get('message')
+        score = request.POST.get('score')
+        question = request.POST.get('question')
+
+        try:
+            course = models.Course.objects.get(slug=self.kwargs['slug'])
+            if message and score:
+                models.CourseComment.objects.create(user=self.request.user, score=score, message=message,
+                                                    course=course, is_publish=True)
+            if question:
+                models.AskedQuestion.objects.create(user=request.user, question=question, course=course,
+                                                    is_publish=True)
+
+            return redirect(reverse('course_app:course_detail', kwargs={'slug': self.kwargs['slug']}))
+        except:
+            raise Http404('')
+
+
+class LikeCourseView(View, LoginRequiredMixin):
     def get(self, request, id):
         try:
             like = models.CourseLikes.objects.get(course_id=id, user_id=self.request.user.id)
             like.delete()
-            return JsonResponse({'response': 'dislike'})
+            response = 'dislike'
         except:
             models.CourseLikes.objects.create(course_id=id, user_id=self.request.user.id)
-            return JsonResponse({'response': 'like'})
+            response = 'like'
+        finally:
+            return JsonResponse({'response': response})
+
+
+class LikeCourseCommentView(View, LoginRequiredMixin):
+    def get(self, request, id):
+        try:
+            like = models.LikesCourseComment.objects.get(comment_id=id, user_id=self.request.user.id)
+            like.delete()
+            response = 'dislike'
+        except:
+            models.LikesCourseComment.objects.create(comment_id=id, user_id=self.request.user.id)
+            response = 'like'
+
+        finally:
+            return JsonResponse({'response': response})
