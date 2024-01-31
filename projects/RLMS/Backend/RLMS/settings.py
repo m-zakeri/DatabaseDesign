682e0fc2594c07d9d1d73d35944159b14dbbc451
vahid <vahid.esmaeeli8282@gmail.com>
2024-01-31 21:51:09 +0330

    Changing database engine to MYSQL engine

--- projects/RLMS/Backend/RLMS/settings.py
+++ projects/RLMS/Backend/RLMS/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = "RLMS.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "RLMS",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
+
 
 
 # Password validation
