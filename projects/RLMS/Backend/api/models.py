2d8a4a32d6c27b2b8b2fb6025eb05a3b70057141
Ghazal <ghazal.alirezaie@gmail.com>
2024-01-31 15:08:32 +0330

    name change

--- projects/RLMS/Backend/api/models.py
+++ projects/RLMS/Backend/api/models.py
@@ -9,7 +9,7 @@ class Person(models.Model):
     gender_types = (("Male", "Male"), ("Female", "Female")) #
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
-    Last_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     father_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=100) #
     address = models.CharField(max_length=500)
@@ -25,80 +25,78 @@ class Person(models.Model):
         return current_time - birth_year
 
     def __str__(self):
-        return self.name + " " + self.family
+        return self.first_namename + " " + self.last_name
 
 
 
 class Email(models.Model):
-    email_ypes = (('Personal', 'Personal'), ('Work', 'Work'), ('University', 'University')) #
-    Email_ID = models.AutoField(primary_key=True)
-    Person_ID = models.ForeignKey(Person, on_delete=models.CASCADE)
-    Email_Address = models.EmailField(max_length=100)
-    Email_Type = models.CharField(max_length=100, choices=Email_Types, default='Personal')
+    email_types = (('Personal', 'Personal'), ('Work', 'Work'), ('University', 'University')) #
+    email_if = models.AutoField(primary_key=True)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+    email_address = models.EmailField(max_length=100)
+    email_type = models.CharField(max_length=100, choices=email_types, default='Personal')
 
     def __str__(self):
-        return self.Email_Address
+        return self.email_address
 
 class Phone(models.Model):
-    Phone_ID = models.AutoField(primary_key=True)
-    Person_ID = models.ForeignKey(Person, on_delete=models.CASCADE)
-    Phone_Number = models.CharField(max_length=11)
+    phone_id = models.AutoField(primary_key=True)
+    person_id  = models.ForeignKey(Person, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=11)
 
     def __str__(self):
-        return self.Phone_Number
+        return self.phone_number
 
 class Student(models.Model):
-    Student_ID = models.AutoField(primary_key=True)
-    Person_ID = models.ForeignKey(Person, on_delete=models.CASCADE)
-    Educational_Degree = models.CharField(max_length=100) #
-    University = models.CharField(max_length=100) #
-    GPA = models.FloatField() #
-    Field = models.CharField(max_length=100)
+    student_id = models.AutoField(primary_key=True)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+    educational_degree = models.CharField(max_length=100) #
+    university = models.CharField(max_length=100) #
+    gpa = models.FloatField() #
+    field = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.Student_Number
+        return self.student_id
 
 class Subscription_plan(models.Model):
-    Student_ID = models.ForeignKey(Student, on_delete=models.CASCADE)
-    Start_Date = models.DateField()
-    Subscription_Duration = models.DuritionField()
-    Price = models.IntegerField()
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    subscription_duration = models.DuritionField()
+    price = models.IntegerField()
 
     @property
     def end_date(self):
-        return self.Start_Date + self.Subscription_Duration
+        return self.start_date + self.subscription_duration
 
-    def __str__(self):
-        return self.Plan_Name
 
 class Course(models.Model):
-    Course_ID = models.AutoField(primary_key=True)
-    Student_ID = models.ForeignKey(Student, on_delete=models.CASCADE)
-    Name = models.CharField(max_length=100)
-    Platform = models.CharField(max_length=100)
-    Subject = models.CharField(max_length=100)
-    Session_Number = models.IntegerField()
-    Estimated_Time = models.DurationField()
-    Description = models.CharField(max_length=500) #
-    Price = models.IntegerField() #
+    course_id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    session_number = models.IntegerField()
+    estimated_time = models.DurationField()
+    description = models.CharField(max_length=500) #
+    price = models.IntegerField() #
 
     def __str__(self):
-        return self.Name
+        return self.name
 
 class Department(models.Model):
-    Department_ID = models.AutoField(primary_key=True)
-    Name = models.CharField(max_length=100)
-    Location = models.Model(max_length=500)
-    Work_Time = models.DurationField()
+    department_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    location = models.Model(max_length=500)
+    work_time = models.DurationField()
 
     def __str__(self):
-        return self.Name
+        return self.name
 
 class Room(models.Model):
-    Room_ID = models.AutoField(primary_key=True)
-    Department_ID = models.ForeignKey(Department, on_delete=models.CASCADE)
-    Name = models.CharField(max_length=100)
+    room_id = models.AutoField(primary_key=True)
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
 
 
     def __str__(self):
-        return self.Name
+        return self.name
