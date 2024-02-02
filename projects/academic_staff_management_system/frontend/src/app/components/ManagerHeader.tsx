68bdf7408345f40add85f30294fbfd1c2886e1e8
Younes Vm <younesvm@gmail.com>
2024-02-02 05:55:45 +0330

    Fixed manage menu component and header bug

--- projects/academic_staff_management_system/frontend/src/app/components/ManagerHeader.tsx
+++ projects/academic_staff_management_system/frontend/src/app/components/ManagerHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ManagerHeader = () => {
   return (
-    <header className="bg-main-peach w-screen flex justify-between px-12 py-4 items-center">
+    <header className="bg-main-peach w-screen flex justify-between px-12 py-4 items-center box-border max-w-[100%]">
       <p>Welcome, ADMIN_FNAME ADMIN_LNAME</p>
       <h1 className="font-bold text-2xl">Administrative Panel</h1>
       <a href="" className="underline">
