8aa7c89e83a7183cec7af9e9d4a0fabac9851c70
Raeinf <54812433+Raeinf@users.noreply.github.com>
2024-02-03 21:58:36 +0330

    Cleaning view

--- projects/RLMS/Backend/laboratory/views.py
+++ projects/RLMS/Backend/laboratory/views.py
@@ -1,25 +1,9 @@
-from django.shortcuts import render, redirect
-from admin_volt.forms import (
-    RegistrationForm,
-    LoginForm,
-    UserPasswordResetForm,
-    UserPasswordChangeForm,
-    UserSetPasswordForm,
-)
-from django.contrib.auth.views import (
-    LoginView,
-    PasswordResetView,
-    PasswordChangeView,
-    PasswordResetConfirmView,
-)
-from django.contrib.auth import logout
-
-from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render
+from .models import *
 
 # Create your views here.
-#
-#
+
+
 def dashboard(request):
     context = {"segment": "dashboard"}
     return render(request, "pages/dashboard/dashboard.html", context)
