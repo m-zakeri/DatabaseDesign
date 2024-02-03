c7fee4361f6e3089081e77b5f4f9ee69fe785c4f
Raeinf <54812433+Raeinf@users.noreply.github.com>
2024-02-03 15:31:50 +0330

    Creating view to overwrite volt view for dashboard

--- projects/RLMS/Backend/laboratory/views.py
+++ projects/RLMS/Backend/laboratory/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from admin_volt.forms import (
+    RegistrationForm,
+    LoginForm,
+    UserPasswordResetForm,
+    UserPasswordChangeForm,
+    UserSetPasswordForm,
+)
+from django.contrib.auth.views import (
+    LoginView,
+    PasswordResetView,
+    PasswordChangeView,
+    PasswordResetConfirmView,
+)
+from django.contrib.auth import logout
+
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+#
+#
+def dashboard(request):
+    context = {"segment": "dashboard"}
+    return render(request, "pages/dashboard/dashboard.html", context)
