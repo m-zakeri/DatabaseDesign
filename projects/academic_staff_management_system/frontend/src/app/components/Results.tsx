43b5939d6660498c8a3a9adc00e65243cc3e0d01
Seyed Amir Hosseini <seyedamirhosseini00@gmail.com>
2024-02-04 11:35:56 +0330

    Some change for getting data

--- projects/academic_staff_management_system/frontend/src/app/components/Results.tsx
+++ projects/academic_staff_management_system/frontend/src/app/components/Results.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
-type Props = {}
+type Props = {
+  persons: (Person & User)[];
+};
 
-export default function Results({}: Props) {
+export default function Results({ persons }: Props) {
   return (
-    <div className='lg:flex-auto'>
+    <div className="lg:flex-auto">
       <h2 className="text-main-highlight-text text-[24px] font-bold">
         Results
       </h2>
       <div className="mt-[15px] flex flex-col gap-[10px]">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {persons.map((person) => (<Card key={person.national_code} person={person}/>))}
       </div>
     </div>
   );
--- projects/academic_staff_management_system/frontend/src/app/page.tsx
+++ projects/academic_staff_management_system/frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import Image from "next/image";
 import Catalog from "./components/Catalog";
 import Filters from "./components/Filters";
 import Results from "./components/Results";
+import getPersonUsers from "../../lib/getPersonUsers";
 
-export default function Home() {
+
+
+export default async function Home() {
+  const persons : (Person & User)[] = await getPersonUsers();
   return (
     <>
       <header>
@@ -11,7 +17,7 @@ export default function Home() {
       </header>
       <main className="px-[30px] pb-[30px] flex flex-col gap-[30px] lg:flex-row lg:max-w-[1300px] mx-auto lg:pt-[128px]">
         <Filters />
-        <Results />
+        <Results persons={persons}/>
       </main>
     </>
   );
